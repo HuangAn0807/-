2c39b456f34bb48248e15dbc964f059ad324a3f9
huang <941432719@qq.com>
2024-08-30 09:01:53 +0800

    测试分支

---
+++ src/components/MyInput.vue
@@ -0,0 +1,15 @@
+<script setup lang='js' name=''>
+defineProps(['modelValue'])
+const emit = defineEmits(['update:modelValue'])
+</script>
+<template>
+
+    <input type="text" :value="modelValue" 
+    @input="emit('update:modelValue',$event.target.value)"
+    >
+
+</template>
+
+<style scoped >
+
+</style>
---
+++ src/components/Select.vue
@@ -0,0 +1,22 @@
+<script setup lang='js' name=''>
+const selectValue = defineModel()
+defineProps(['modelValue','options'])
+const emit = defineEmits(['update:modelValue'])
+</script>
+<template>
+  <div >
+    <!-- 写法一使用prop和自定义事件 -->
+    <!-- <select :value="modelValue" @change="emit('update:modelValue',$event.target.value)" >
+      <option v-for="item in options" :value="item.value" :label="item.label"></option>
+    </select> -->
+
+    <!-- 写法二使用defineModel实现 -->
+    <select v-model="selectValue" >
+      <option v-for="item in options" :value="item.value" :label="item.label"></option>
+    </select>
+</div>
+</template>
+
+<style scoped >
+
+</style>
